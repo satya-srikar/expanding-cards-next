887dd07405c6120e85e43917e64731f329206f25
satya-srikar <srikar.n@valueinfotech.com>
2024-10-09 14:54:32 -0700

    setup github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
+  output: isProd ? "export" : "",
   reactStrictMode: true,
   assetPrefix: isProd ? "/expanding-cards-next/" : "",
   basePath: isProd ? "/expanding-cards-next" : "",
